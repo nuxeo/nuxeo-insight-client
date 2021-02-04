0ade31ca1e352e3e0d1066b8d53fd1b83cd25e4e
Julien Carsique <jcarsique@nuxeo.com>
2021-02-04 15:17:42 +0100

    ci(INSIGHT-656): init pipeline & DevTools
    Jenkinsfile main pipeline
    Tools: release pipeline, sprint rollout

---
+++ release.groovy
@@ -0,0 +1,67 @@
+/*
+ * (C) Copyright 2021 Nuxeo (http://nuxeo.com/).
+ * This is unpublished proprietary source code of Nuxeo SA. All rights reserved.
+ * Notice of copyright on this source code does not indicate publication.
+ *
+ * Contributors:
+ *     Julien Carsique <jcarsique@nuxeo.com>
+ */
+
+void setGitHubBuildStatus(String context) {
+    step([
+            $class       : 'GitHubCommitStatusSetter',
+            reposSource  : [$class: 'ManuallyEnteredRepositorySource', url: 'https://github.com/nuxeo/nuxeo-insight-client'],
+            contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: context],
+            errorHandlers: [[$class: 'ShallowAnyErrorHandler']]
+    ])
+}
+
+pipeline {
+    agent {
+        label "jenkins-ai-nuxeo11"
+    }
+    parameters {
+        string(name: 'BRANCH', defaultValue: 'master', description: 'Branch to release from.')
+        string(name: 'VERSION', description: '''The version to release.
+Leave it unset to use the version of the Maven POM.
+The version must match "X.Y.Z[-PRERELEASE][+BUILD]"''')
+        choice(name: 'INCREMENT', choices: ['patch', 'minor', 'major', 'release'], description: '''Semantic versioning increment.
+The "release" increment mode removes any PRERELEASE or BUILD parts (see VERSION).''')
+        booleanParam(name: 'DRY_RUN', defaultValue: true, description: 'Dry run')
+    }
+    options {
+        disableConcurrentBuilds()
+        buildDiscarder(logRotator(numToKeepStr: '10', artifactNumToKeepStr: '3'))
+        timeout(time: 1, unit: 'HOURS')
+    }
+    stages {
+        stage('Release') {
+            steps {
+                setGitHubBuildStatus('release')
+                container('platform11') {
+                    withEnv(["BRANCH=${params.BRANCH}", "VERSION=${params.VERSION}", "INCREMENT=${params.INCREMENT}",
+                             "DRY_RUN=${params.DRY_RUN}"]) {
+                        script {
+                            sh './tools/release.sh'
+                            sh 'chmod +r VERSION || true'
+                            if ("$DRY_RUN" != 'true') {
+                                def releaseProps = readProperties file: 'release.properties'
+                                def jobName = '/nuxeo/nuxeo-ai/v' + releaseProps['RELEASE_VERSION']
+                                if (!Jenkins.instance.getItemByFullName(jobName)) {
+                                    build job: '/nuxeo/nuxeo-insight-client/', propagate: false, wait: true
+                                }
+                                build job: jobName, propagate: false, wait: false
+                            }
+                        }
+                    }
+                }
+            }
+            post {
+                always {
+                    setGitHubBuildStatus('release')
+                    archiveArtifacts artifacts: 'release.properties', allowEmptyArchive: true
+                }
+            }
+        }
+    }
+}
